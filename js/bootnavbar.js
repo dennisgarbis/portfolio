7ae87dd531476fc40fc3e88716a1bcea4774fbf7
Dennis Garbis <dennisgarbis107@gmail.com>
2022-06-04 13:55:21 -0500

    added comments

--- js/bootnavbar.js
+++ js/bootnavbar.js
@@ -49,3 +49,16 @@ $('.dropdown-menu a.dropdown-toggle').on('click', function(e) {
   });
   return false;
 });
+
+// document.querySelector('.first-button').addEventListener('click', function () {
+
+//   document.querySelector('.animated-icon1').classList.toggle('open');
+//   });
+//   document.querySelector('.second-button').addEventListener('click', function () {
+  
+//   document.querySelector('.animated-icon2').classList.toggle('open');
+//   });
+//   document.querySelector('.third-button').addEventListener('click', function () {
+  
+//   document.querySelector('.animated-icon3').classList.toggle('open');
+//   });
